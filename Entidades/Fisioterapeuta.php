86126f054f9d4a31572ed9e426d45f79f86ad629
CORHUILA <CORHUILA@PC4>
2016-11-08 19:57:56 -0500

    fisioterapeuta

---
+++ Entidades/Fisioterapeuta.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Fisioterapeuta
+ *
+ * @author CORHUILA
+ */
+class Fisioterapeuta {
+    //put your code here
+}
